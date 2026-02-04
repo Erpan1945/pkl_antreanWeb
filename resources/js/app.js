c5d71af76fd78b332db8c11fd3e62bec1265df95
Daniel <irfan2904.ia@gmail.com>
2026-02-04 09:56:59 +0700

    fix depedensi ziggy

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,12 @@ import './bootstrap';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+
+// --- PERUBAHAN DI SINI ---
+// 1. Ambil library dari NPM (bukan folder vendor)
+import { ZiggyVue } from 'ziggy-js';
+// 2. Ambil daftar route yang sudah di-generate tadi
+import { Ziggy } from './ziggy';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -18,7 +23,8 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
-            .use(ZiggyVue)
+            // 3. Masukkan konfigurasi route ke plugin
+            .use(ZiggyVue, Ziggy) 
             .mount(el);
     },
     progress: {
